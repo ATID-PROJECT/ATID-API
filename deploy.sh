4e4e57e06ef2aaab3cbf4a464c9f55338d0d76ab
unknown <mateusapb@live.com>
2019-01-06 10:29:08 -0200

    deploy config

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-
 pipenv --rm
-make install
+pipenv install
 echo "start flask-app"
 pipenv run gunicorn -b 0.0.0.0:5000 --access-logfile - "run:create_app()"
