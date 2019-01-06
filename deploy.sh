49083b74c5972d17b6bbf95a8fbbe216a383c0aa
unknown <mateusapb@live.com>
2019-01-06 17:51:50 -0200

    finished deploy config and added thread process for gunicorn instance

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ pipenv --rm
 make install
 echo "start flask-app"
 fuser -k 5000/tcp
-pipenv run gunicorn -b 0.0.0.0:5000 --access-logfile - "run:create_app()"
+pipenv run gunicorn -b 0.0.0.0:5000 --access-logfile - "run:create_app()" --daemon
