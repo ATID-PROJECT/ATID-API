0259f4507ae499881c3260ecdf4f65bd6e4ee1d2
Mateus-Brito <mateusapb@live.com>
2019-08-02 09:44:33 -0300

    refresh

--- app/general/events/user_enrolment.py
+++ app/general/events/user_enrolment.py
@@ -9,29 +9,35 @@ from .util import *
 
 def UserToStart(db, url_host, id_course, id_user):
     
-    network = db.run( f"MATCH (network:Network{{url:'{url_host}'}})-[r2]-(c:Course{{id:{id_course}}}) return network").data()[0]['network']
-    all_data = json.loads( network['all_data'])
+    try:
 
-    for item in all_data:
-        if( 'id' in item and item['type'].lower() == "custom:start"):
-            
-            target_transictions,target_activitys = getNextActivity(db, url_host, id_course, item['id'])
+        network = db.run( f"MATCH (network:Network{{url:'{url_host}'}})-[r2]-(c:Course{{id:{id_course}}}) return network").data()[0]['network']
+        all_data = json.loads( network['all_data'])
 
-            target_item = getNetworkByID(db, url_host, id_course, target_activitys[0])
-            
-            if not('suggestion_uuid' in target_item):
-                abort(400, {'message': 'A rede possuí erros estruturais.'})
+        for item in all_data:
+            if( 'id' in item and item['type'].lower() == "custom:start"):
+                
+                target_transictions,target_activitys = getNextActivity(db, url_host, id_course, item['id'])
 
-            
-            target_uuid = target_item['suggestion_uuid']
-            
-            target_activity = db.run(f"MATCH (network:Network{{url:'{url_host}'}})-[:HAS_QUESTIONS]->(target{{uuid:'{target_uuid}'}}) return target").data()[0]['target']
-            id_group = getInstanceOfActivityUUID(db, url_host, id_course, target_uuid, target_activity['label'])[0]["instance"]["id_group"]
-             
-            addUserToGroup(url_host, network['token'], str(id_user), str(id_group))
+                target_item = getNetworkByID(db, url_host, id_course, target_activitys[0])
+                
+                if not('suggestion_uuid' in target_item):
+                    abort(400, {'message': 'A rede possuí erros estruturais.'})
 
-            return
+                
+                target_uuid = target_item['suggestion_uuid']
+                
+                target_activity = db.run(f"MATCH (network:Network{{url:'{url_host}'}})-[:HAS_QUESTIONS]->(target{{uuid:'{target_uuid}'}}) return target").data()[0]['target']
+                id_group = getInstanceOfActivityUUID(db, url_host, id_course, target_uuid, target_activity['label'])[0]["instance"]["id_group"]
+                
+                addUserToGroup(url_host, network['token'], str(id_user), str(id_group))
 
+                return
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno, file=sys.stderr)
+        return 400
 
 
     
